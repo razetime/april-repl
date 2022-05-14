91d422e803b4b76ad3746ae85eea2ab0434da7da
razetime <raghuallthetime@hotmail.com>
2022-05-14 06:46:11 +0530

    add symbols cmd

--- repl.lisp
+++ repl.lisp
@@ -10,8 +10,55 @@
 ]defns - All definitions in the current workspace so far
 ]save [file] - Save REPL history in 'file'. If unspecified, will save in temp.apl in the current folder.
 ]off - Close REPL.")
-
 (defconstant NL (format nil "~C" #\newline))
+(defconstant SYMS
+"← ASSIGN                       ⍷ find                                      
+                               ∪ unique union                              
++ conjugate plus               ∩ intersection                              
+- negate minus                 ~ not without                               
+× direction times                                                          
+÷ reciprocal divide            / replicate Reduce                          
+* exponential power            \ \expand Scan                              
+⍟ natural logarithm logarithm  ⌿ replicate first Reduce First              
+⌹ matrix inverse matrix divide ⍀ expand first Scan First                   
+○ pi times circular                                                        
+! factorial binomial           , ravel catenate/laminate                   
+? roll deal                    ⍪ table catenate first/laminate             
+                               ⍴ shape reshape                             
+| magnitude residue            ⌽ reverse rotate                            
+⌈ ceiling maximum              ⊖ reverse first rotate first                
+⌊ floor minimum                ⍉ transpose reorder axes                    
+⊥ decode                                                                   
+⊤ encode                       ¨ Each                                      
+⊣ same left                    ⍨ Constant Self Swap                        
+⊢ same right                   ⍣ Repeat Until                              
+                               . Outer Product (∘.) Inner Product          
+= equal                        ∘ OUTER PRODUCT (∘.) Bind Beside            
+≠ unique mask not equal        ⍤ Rank Atop                                 
+≤ less than or equal to        ⍥ Over                                      
+< less than                    @ At                                        
+> greater than                                                             
+≥ greater than or equal to     ⍞ STDIN STDERR                              
+≡ depth match                  ⎕ EVALUATED STDIN STDOUT SYSTEM NAME PREFIX 
+≢ tally not match              ⍠ Variant                                   
+                               ⌸ Index Key Key                             
+∨ greatest common divisor/or   ⌺ Stencil                                   
+∧ lowest common multiple/and   ⌶ I-Beam                                    
+⍲ nand                         ⍎ execute                                   
+⍱ nor                          ⍕ format                                    
+                                                                           
+↑ mix take                     ⋄ STATEMENT SEPARATOR                       
+↓ split drop                   ⍝ COMMENT                                   
+⊂ enclose partioned enclose    → ABORT BRANCH                              
+⊃ first pick                   ⍵ RIGHT ARGUMENT RIGHT OPERAND (⍵⍵)         
+⊆ nest partition               ⍺ LEFT ARGUMENT LEFT OPERAND (⍺⍺)           
+⌷ materialise index            ∇ recursion Recursion (∇∇)                  
+⍋ grade up grades up           & Spawn                                     
+⍒ grade down grades down                                                   
+                               ¯ NEGATIVE                                  
+⍳ indices indices of           ⍬ EMPTY NUMERIC VECTOR                      
+⍸ where interval index         ∆ IDENTIFIER CHARACTER                      
+∊ enlist member of             ⍙ IDENTIFIER CHARACTER                      ")
 (defvar *defns* "")
 
 (write-string "April Compiler REPL
@@ -23,7 +70,7 @@ Type ]off to exit
   do (let ((input (read-line)))
        (if (eql #\] (char input 0))
            (cond  ((string= "]help"    input) (write-line HELP))
-                  ((string= "]symbols" input) (write-line "NYI")) ; TODO
+                  ((string= "]symbols" input) (write-line SYMS))
                   ((string= "]defns"   input) (write-line *defns*))
                   ((string= "]off"     input) (write-line "Exiting April...") (setf *running* nil))
                   ((string= "]save" (subseq input 0 5)) 
